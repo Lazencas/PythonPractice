a3d27d64298235a0a7059af5b568ed1cdb7bf173
Linyera <57083072+Lazencas@users.noreply.github.com>
2022-03-26 14:33:20 +0900

    1 commit

---
+++ 0325-6prac1.py
@@ -0,0 +1,31 @@
+#코드업100제 문제 풀이 1~8
+#print("print(\"Hello\\nWorld\")")
+
+#
+#변수는 수나 문자등의 어떤 값을 저장할 수 있는 공간. 숫자로 시작하는 변수이름은 사용 불가, 수로 인식함.
+#@@ 9번
+# v = input()
+# print(v)
+
+#@@ 10번 
+#숫자 0-9 와 소수점을 사용해 표현한 수를 실수라고 한다.
+# n = input()
+# n = int(n)
+# print(n)
+
+#@@ 11번
+# f = input()
+# f = float(f)
+# print(f)
+
+#@@ 12번
+# a = input()
+# b = input()
+# print(a+"\n"+b)
+
+#@@ 13번
+a = input()
+b = input()
+print(b+"\n"+a)
+
+
