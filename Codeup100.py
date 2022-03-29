40821eee598edf566f92ee336fc85f8b575d5b84
Linyera <57083072+Lazencas@users.noreply.github.com>
2022-03-30 06:37:21 +0900

    5commit

--- Codeup100.py
+++ Codeup100.py
@@ -175,3 +175,16 @@
 #@@ 53번
 # n = bool(int(input()))
 # print(not n)
+
+#@@@ 22년 3월 30일
+#@@ 54번 2개의 정수값이 입력될 때, 그 불 값이 모두 True 일 때에만 True 를 출력하는 프로그램을 작성해보자.
+# a, b= input().split()
+# print(bool(int(a)) and bool(int(b)))
+
+#@@ 56번 2개의 정수값이 입력될 때, 그 불 값(True/False) 이 서로 다를 때에만 True 를 출력하는 프로그램을 작성해보자. &&&&&&
+a, b = input().split()
+c = bool(int(a))
+d = bool(int(b))
+print((c and(not d))or((not c)and d))
+
+
